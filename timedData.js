e8329f14da3a26c42f5bef2aae60f8e08c50cc12
ShaylanDias <sdias695@student.fuhsd.org>
2018-10-02 16:18:49 -0700

    Ye

---
+++ timedData.js
@@ -0,0 +1,31 @@
+{
+    // Timed data
+    var array = [
+        { data: 'A', start: 0, end: 1, r: 0, g: 1, b: 0.5 },
+        { data: 'B', start: 2, end: 3, r: 10, g: 1, b: 0.5 },
+        { data: 'C', start: 4, end: 5, r: 40, g: 1, b: 0.5 },
+        { data: 'D', start: 6, end: 7, r: 0, g: 60, b: 0.5 },
+        { data: 'E', start: 8, end: 9, r: 0, g: 1, b: 90 },
+        { data: 'F', start: 10, end: 11, r: 0, g: 1, b: 0.5 },
+        { data: 'G', start: 12, end: 13, r: 0, g: 1, b: 0.5 },
+        { data: 'H', start: 14, end: 15, r: 0, g: 1, b: 0.5 },
+        { data: 'I', start: 16, end: 17, r: 0, g: 1, b: 0.5 },
+        { data: 'J', start: 18, end: 19, r: 0, g: 1, b: 0.5 },
+        { data: 'K', start: 20, end: 21, r: 0, g: 1, b: 0.5 },
+        { data: 'L', start: 22, end: 23, r: 0, g: 1, b: 0.5 },
+        { data: 'M', start: 24, end: 25, r: 0, g: 1, b: 0.5 },
+        { data: 'N', start: 26, end: 27, r: 0, g: 1, b: 0.5 },
+        { data: 'O', start: 28, end: 29, r: 0, g: 1, b: 0.5 },
+        { data: 'P', start: 30, end: 31, r: 0, g: 1, b: 0.5 },
+        { data: 'Q', start: 32, end: 33, r: 0, g: 1, b: 0.5 },
+        { data: 'R', start: 34, end: 35, r: 0, g: 1, b: 0.5 },
+        { data: 'S', start: 36, end: 37, r: 0, g: 1, b: 0.5 },
+        { data: 'T', start: 38, end: 39, r: 0, g: 1, b: 0.5 },
+        { data: 'U', start: 40, end: 41, r: 0, g: 1, b: 0.5 },
+        { data: 'V', start: 42, end: 43, r: 0, g: 1, b: 0.5 },
+        { data: 'W', start: 44, end: 45, r: 0, g: 1, b: 0.5 },
+        { data: 'X', start: 46, end: 47, r: 0, g: 1, b: 0.5 },
+        { data: 'Y', start: 48, end: 49, r: 0, g: 1, b: 0.5 },
+        { data: 'Z', start: 50, end: 51, r: 0, g: 1, b: 0.5 } 
+    ];
+}
